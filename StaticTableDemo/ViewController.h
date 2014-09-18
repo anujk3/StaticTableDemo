0f04abf57da5bdab70b9b6e51ca56bbab8204270
Anuj Katiyal <anujk3@gmail.com>
2014-09-18 15:04:42 +0530

    Initial Commit

---
+++ StaticTableDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StaticTableDemo
+//
+//  Created by Anuj Katiyal on 18/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StaticTableDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StaticTableDemo
+//
+//  Created by Anuj Katiyal on 18/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
